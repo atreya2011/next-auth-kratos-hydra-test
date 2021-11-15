418fbe96c5c32d7c056776b511a6f42074d6754c
Atreya <44151328+atreya2011@users.noreply.github.com>
2021-11-15 09:33:18 +0900

    fix(deps): update packages and pin dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
       jsx: true, // Allows for the parsing of JSX
     },
   },
-  ignorePatterns: ["!.prettierrc.js", "!.lintstagedrc.js", "!.huskyrc.js"], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
+  ignorePatterns: ["!.prettierrc.js", "!.lintstagedrc.js"], // We don't want to lint generated files nor node_modules, but we want to lint .prettierrc.js (ignored by default by eslint)
   settings: {
     react: {
       version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
@@ -26,17 +26,20 @@ module.exports = {
     "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
     "plugin:react-hooks/recommended",
     "plugin:jsx-a11y/recommended",
-    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "prettier",
     "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "next",
   ],
   // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
   rules: {
     "prettier/prettier": ["error", {}, { usePrettierrc: true }],
     "react/react-in-jsx-scope": "off", // No need to import React when using Next.js
     "react/prop-types": "off", // We will use TypeScript's types for component props instead
-    "@typescript-eslint/no-unused-vars": ["error"], // Why would you want unused vars?
+    "@typescript-eslint/no-unused-vars": ["error", { ignoreRestSiblings: true }], // Why would you want unused vars?
     "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/explicit-function-return-type": "off",
+    "jsx-a11y/no-onchange": "off",
+    "@next/next/no-img-element": "off",
     // This rule is not compatible with Next.js's <Link /> components
     "jsx-a11y/anchor-is-valid": [
       "error",
--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "*": (filenames) => `npm run format-files ${filenames.join(" ")}`, // Run prettier on all staged files.
+  "**/*.{tsx,ts,jsx,js,json,md,css}": (filenames) => `npm run format-files ${filenames.join(" ")}`, // Run prettier on all staged files.
   "**/*.ts?(x)": () => "npm run type-check", // Run type-check on changes to TypeScript files.
   "**/*.(ts|js)?(x)": (filenames) => `npm run lint-files ${filenames.join(" ")}`, // Run ESLint on changes to JavaScript/TypeScript files.
 };
